df5a5f4f284cea5142648fd64585d1b0264269c4
Steve Roberts <mbudm.com@gmail.com>
2019-09-05 20:55:01 +1000

    Bye bye old wordpress site forever

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Yo!
+# mbudm.com source
+
+The source for my [personal site](https://mbudm.com), using Gatsby JS.
--- public/clients/playup/prototypes/v1_iteration1/data/configuration.js
+++
@@ -1,5 +0,0 @@
-﻿var configuration = 
-{
-"showPageNotes":true,
-"logoImageCaption":"PlayUp Version 1.0",
-"loadFeedbackPlugin":false}
